fe43a3869443f415e43392df6a478972d3d4f1a4
Karol Moroz <karol@moroz.dev>
2024-03-21 00:41:54 +0800

    WIP on password authentication

--- service/user_service.go
+++ service/user_service.go
@@ -1,5 +1,19 @@
 package service
 
-type userService struct {
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/moroz/goma/store"
+	"github.com/moroz/goma/types"
+)
 
+type UserService struct {
+	store store.UserStore
+}
+
+func NewUserService(db *sqlx.DB) UserService {
+	return UserService{store: store.NewUserStore(db)}
+}
+
+func AuthenticateUserByEmailPassword(email, password string) (*types.User, error) {
+	var user types.User
 }
--- types/user.go
+++ types/user.go
@@ -1,6 +1,11 @@
 package types
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+)
 
 type User struct {
 	ID           int       `db:"id"`
@@ -8,4 +13,11 @@ type User struct {
 	PasswordHash *string   `db:"password_hash"`
 	InsertedAt   time.Time `db:"inserted_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
+}
+
+func (u *User) CheckPassword(password string) (match bool, err error) {
+	if u.PasswordHash == nil || u.PasswordHash == "" {
+		return false, errors.New("no password set for the user")
+	}
+	match, err = argon2id.ComparePasswordAndHash()
 }
