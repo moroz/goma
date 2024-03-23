26b1af03e3df1825353397fd47203580b1123ea1
Karol Moroz <karol@moroz.dev>
2024-03-24 01:13:10 +0800

    Tweak test cases

--- service/user_service.go
+++ service/user_service.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"errors"
-
 	"github.com/jmoiron/sqlx"
 	"github.com/moroz/goma/store"
 	"github.com/moroz/goma/types"
@@ -16,20 +14,15 @@ func NewUserService(db *sqlx.DB) UserService {
 	return UserService{store: store.NewUserStore(db)}
 }
 
-var InvalidPasswordError = errors.New("invalid password")
-
 func (us *UserService) AuthenticateUserByEmailPassword(email, password string) (*types.User, error) {
 	user, err := us.store.GetUserByEmail(email)
 	if err != nil {
 		return nil, err
 	}
 
-	match, err := user.CheckPassword(password)
-	if err != nil {
+	if _, err := user.CheckPassword(password); err != nil {
 		return nil, err
 	}
-	if !match {
-		return nil, InvalidPasswordError
-	}
+
 	return user, nil
 }
